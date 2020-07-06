8b605939990ec6f4e1cc8590029064b861562d30
aybamzy-git <67778766+aybamzy-git@users.noreply.github.com>
2020-07-06 20:21:11 +0100

    Add files via upload

---
+++ js/fade.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container1', {
+      spaceBetween: 30,
+      effect: 'fade',
+      pagination: {
+        el: '.swiper-pagination1',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
